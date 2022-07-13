096b50d275268014f2e97c7c9446b2398fdf6a80
Douglas Christopher Wilson <doug@somethingdoug.com>
2022-07-12 22:51:06 -0400

    wip

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ function finalhandler (req, res, options) {
     // cannot actually respond
     if (headersSent(res)) {
       debug('cannot %d after headers sent', status)
-      req.socket.destroy()
+      req.destroy()
       return
     }
 
